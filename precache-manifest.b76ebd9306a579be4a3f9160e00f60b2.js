0d0b4fdb358841f749530402e3d72db0378eb07d
Lisianthus-A <lisianthus.zeng@gmail.com>
2020-05-23 16:03:16 +0800

    Updates

---
+++ precache-manifest.b76ebd9306a579be4a3f9160e00f60b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a24c8ae79813dfe7473d794e825070e",
+    "url": "/demo-cnode/index.html"
+  },
+  {
+    "revision": "b8a41f243f943e8a97ef",
+    "url": "/demo-cnode/static/css/main.404eef83.chunk.css"
+  },
+  {
+    "revision": "4c90a7e4f367f8fb77aa",
+    "url": "/demo-cnode/static/js/2.0078c333.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo-cnode/static/js/2.0078c333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8a41f243f943e8a97ef",
+    "url": "/demo-cnode/static/js/main.db5441af.chunk.js"
+  },
+  {
+    "revision": "1973995faee33674381f",
+    "url": "/demo-cnode/static/js/runtime-main.e8597bab.js"
+  },
+  {
+    "revision": "2380f055451dbfc41cca8ecd4205f51e",
+    "url": "/demo-cnode/static/media/cnode.2380f055.svg"
+  }
+]);
