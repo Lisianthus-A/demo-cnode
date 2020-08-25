cd7c8855e2772fafd9a17f11e67334b4ba4baf55
lisianthus-a <1419033252@qq.com>
2020-08-25 12:00:39 +0800

    change

--- src/App.js
+++ src/App.js
@@ -5,22 +5,20 @@ import Main from './component/Main';
 import Post from './component/Post';
 import User from './component/User';
 
-class App extends React.Component {
-
-  render() {
-    return (
-      <HashRouter>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={Main} />
-          <Route exact path='/post' component={() => <Redirect to='/' />} />
-          <Route exact path='/user' component={() => <Redirect to='/' />} />
-          <Route path='/post/:id' component={Post} />
-          <Route path='/user/:name' component={User} />
-        </Switch>
-      </HashRouter>
-    );
-  }
+const App = props => {
+  
+  return (
+    <HashRouter>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Main} />
+        <Route exact path='/post' component={() => <Redirect to='/' />} />
+        <Route exact path='/user' component={() => <Redirect to='/' />} />
+        <Route path='/post/:id' component={Post} />
+        <Route path='/user/:name' component={User} />
+      </Switch>
+    </HashRouter>
+  );
 }
 
 export default App;
